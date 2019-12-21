46904f982679710e19b0cf0bcc58668dfacd7953
kowalix007 <kowalix007@gmail.com>
2019-12-21 18:43:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts "#{seasons.to_s.capitalize}:" 
     holidays.each do |holiday, supplies|
-    puts " #{holiday.to_s.split("_").collect {|word| word.capitalise}}: #{supplies.join(", ")}" 
+    puts " #{holiday.to_s.split("_").collect {|word| word.capitalize}}: #{supplies.join(", ")}" 
     
    end
  end
